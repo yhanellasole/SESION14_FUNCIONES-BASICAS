ef87fbe48cb9dc329d8dd91f2257dcd30ba61f27
yhanellasole <yhanella.lagos.s@gmail.com>
2022-11-16 22:05:02 -0500

    SE AGREGA EL PROYECTO

---
+++ sesion14.js
@@ -0,0 +1,49 @@
+/* SESION 14
+
+EJERCICIO 01 
+
+let fem=+prompt("Ingrese la cantidad de mujeres:");
+let mas=+prompt("Ingrese la cantidad de varones:");
+
+mujeres= (fem / (fem + mas))* 100 
+hombres= (mas / (fem + mas))* 100
+
+alert('El porcentaje de mujeres es de :' + " "+ mujeres + '%');
+alert('El porcentaje de hombres es de :' + " "+ hombres + '%');
+*/ 
+
+/*EJERCICIO 02
+
+let total=+prompt("Ingrese el monto total de la inversión:");
+
+lamar= total* 0.35;
+abancay= total *0.25;
+julio= total * 0.1;
+aviacion = total *0.15;
+tacna= total - (lamar + abancay + julio + aviacion)
+
+alert('La avenida La Mar recibira S/' + " "+ lamar);
+alert('La avenida Abancay recibira S/' + " "+ abancay);
+alert('La avenida 28 de Julio recibira S/' + " "+ julio);
+alert('La avenida Aviación recibira S/' + " "+ aviacion);
+alert('La avenida Tacna recibira S/' + " "+ tacna);
+*/
+
+/*EJERCICIO 03
+
+let entradageneral= +prompt("Ingrese la cantidad de entradas general:");
+let entrada6= +prompt("Ingrese la cantidad de entradas para mayores de 65 años:");
+let entradamenor= +prompt("Ingrese la cantidad de entradas para menores de edad:");
+
+entradaG= entradageneral * 150
+entradamayor= entrada6 * 50
+entradam= entradamenor * 80
+monto= entradaG + entradamayor + entradam
+
+
+alert('Cantidad recaudada de entradas generales:'+ " " + entradaG);
+alert('Cantidad recuadada de entradas para mayores de 65 años:' + " " + entradamayor);
+alert('Cantidad recuadada de entradas para menores de edad:' + " " + entradam);
+alert('El monto final recuadado:'+" " + monto);
+
+*/
